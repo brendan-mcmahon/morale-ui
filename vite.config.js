676ba8b24b61e3501a384192e60a0d24da88d84f
brendanjeffreymcmahon <brendan.mcmahon@bloomerang.com>
2023-04-10 15:48:48 -0400

    update for ghpages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const base = process.env.NODE_ENV === "production" ? "/morale-ui/" : "/";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base,
+});
